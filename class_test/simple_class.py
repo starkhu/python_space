5dec3a60f10dec51f75b62fcb19b0f5546e8c2eb
Answer <strak@mail.ustc.deu.cn>
2019-05-03 23:46:46 +0800

    add simple class test 2

--- class_test/simple_class.py
+++ class_test/simple_class.py
@@ -1,10 +1,21 @@
 #@property: 用法1：将类方法转换成类属性
 #           用法2：当做装饰函数来用
+#__slot__: 用来限定当前类（不包括其子类）只能绑定限制的属性
+#          如果不加slot, 类可以动态添加属性
+#super(): 给父节点同名函数传参。
+#@staticmethod: 类的静态方法，给类调用的函数，不是给对象调用的函数
 
 class Person(object):
+    __slots__ = ('_name', '_age', '_gender')
     def __init__(self, name, age):
         self._name = name
         self._age = age
+
+    @staticmethod
+    def is_valid_age(age):
+        if age < 0:
+            raise ValueError("valid age!")
+
 
     @property
     def full_name(self):
@@ -24,6 +35,10 @@ class Person(object):
         else:
             raise ValueError("illeage input")
 
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self._grade = grade
 
 
 def attr_test(tony):
@@ -41,10 +56,21 @@ def property_test(tony):
     tony.set_age = 90
     print(tony.set_age)
 
+def slot_test(tony):
+    tony._gender = "man"
+    print(tony._gender)
+    #tony._height = 180 #wrong property
+
+
 def main():
-    tony = Person("Tony", 35)
+    #age = -10
+    #if Person.is_valid_age(age):
+    #   cook = Person("Cook", age)
+
+    tony = Person("Tony", 25)
     #attr_test(tony)
-    property_test(tony)
+    #property_test(tony)
+    slot_test(tony)
 
 
 if __name__ == "__main__":
